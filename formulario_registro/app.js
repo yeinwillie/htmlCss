efc79b1c18c3607086777a642e9c0155efeaa5f7
Yein E <yeinneg@gmail.com>
2023-06-15 22:35:55 -0300

    formulario de registro parcial

---
+++ formulario_registro/app.js
@@ -0,0 +1,18 @@
+// Captura de elementos
+
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const mensajeInput = document.querySelector("#mensaje");
+const errorNode = document.querySelectorAll(".error");
+
+// validar datos
+
+function validarDatos() {    
+
+    console.log("hola")
+
+   /* if (nameInput.value.length < 1) {
+        errorNode[0].innerText = "Campo nombre no puede estar vacio";
+        nameInput.classList.add("error-border");
+    }*/
+}

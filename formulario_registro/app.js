d9fb00bd391a51e075743d851eb67e951ee0412d
Yein E <yeinneg@gmail.com>
2023-06-20 17:24:22 -0300

    formulario listo

--- formulario_registro/app.js
+++ formulario_registro/app.js
@@ -13,6 +13,14 @@ const expresiones = {
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,	
 }
 
+// objeto usado en la Funcion que verifica si los campos estan llenos
+
+const campos = {
+    nombre: false,
+    email: false,
+    mensaje: false
+}
+
 // Funcion que controla el flujo del formulario
 
 const validarFormulario = (e) => {
@@ -39,7 +47,8 @@ const validarCampo = (expresion, input , campo ) => {
         document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-correcto");
         document.querySelector(`#grupo__${campo} i`).classList.remove("fa-circle-xmark");
         document.querySelector(`#grupo__${campo} i`).classList.add("fa-circle-check");
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");                
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");   
+        campos[campo] = true;            
 
     } else {
         document.getElementById(`grupo__${campo}`).classList.add("formulario__grupo-incorrecto");
@@ -47,26 +56,29 @@ const validarCampo = (expresion, input , campo ) => {
         document.querySelector(`#grupo__${campo} i`).classList.remove("fa-circle-check");
         document.querySelector(`#grupo__${campo} i`).classList.add("fa-circle-xmark");
         document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
+        campos[campo] = false; 
     }
-}
+};
 
 //tipo textarea
 
 const validarFormularioTextarea =  () => {
-    if (textarea.value.length ===0) {
-        document.querySelector("#grupo__mensaje .formulario__input-error").classList.add("formulario__input-error-activo");
+    if (textarea.value.length !=0) {      
+        document.querySelector("#grupo__mensaje .formulario__input-error").classList.remove("formulario__input-error-activo");
+        campos.mensaje = true;  
 
     } else {
-        document.querySelector("#grupo__mensaje .formulario__input-error").classList.remove("formulario__input-error-activo");
+        document.querySelector("#grupo__mensaje .formulario__input-error").classList.add("formulario__input-error-activo");
+        campos.mensaje = false;   
     }
-}
+};
 
 // Funcion que escucha los eventos keyup y blur en los campos
 // eventos en input
 inputs.forEach((input)=>{
     input.addEventListener("keyup", validarFormulario );
     input.addEventListener("blur", validarFormulario  );
-})
+});
 
 // eventos en textarea
 textarea.addEventListener("keyup", validarFormularioTextarea);                                              
@@ -76,4 +88,24 @@ textarea.addEventListener("blur", validarFormularioTextarea)
 
 formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
-})
+    
+    if (campos.nombre && campos.email && campos.mensaje) {
+        formulario.reset();
+
+        document.getElementById("formulario__mensaje-exito").classList.add("formulario__mensaje-exito-activo");
+        
+        setTimeout (()=>{
+            document.getElementById("formulario__mensaje-exito").classList.remove("formulario__mensaje-exito-activo");
+        }, 3000)
+
+        document.querySelectorAll(".formulario__grupo-correcto").forEach((icono)=>{
+            icono.classList.remove("formulario__grupo-correcto");
+        });
+    } else {
+        document.getElementById("formulario__mensaje").classList.add("formulario__mensaje-activo");
+        setTimeout (()=>{
+            document.getElementById("formulario__mensaje").classList.remove("formulario__mensaje-activo");
+        }, 3000)
+    }
+
+});
